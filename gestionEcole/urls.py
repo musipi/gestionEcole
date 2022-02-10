cfa930fc2dda5dd67507ab59a8d930411e994e14
chris <chris@isicom.education>
2022-02-10 14:57:29 +0100

    deuxieme commit

--- gestionEcole/urls.py
+++ gestionEcole/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('gestion', include('inscription.urls')),
+    path('inscription/', include('inscription.urls')),
     path('admin/', admin.site.urls),
 ]
